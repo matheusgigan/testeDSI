516567d43cda0631bcc3db8a227870840453510d
matheusgigan <matheusvasconcelosaraujo@gmail.com>
2024-11-28 19:47:29 -0300

    arrumar pasta

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configure a pasta pública para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal (ou outras rotas)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Inicie o servidor
+const PORT = 3000; // ou qualquer porta que desejar
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
