69951313071d98094cb3b0f0d87bfe18acdee139
matheusgigan <matheusvasconcelosaraujo@gmail.com>
2024-11-28 10:23:07 -0300

    teste

---
+++ Aula3/projetoWeb/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Projeto Web - Grupo X está rodando");
+
+    // Alunos: Adicione interações com o DOM, eventos e lógica do projeto aqui
+
+    // Esse arquivo será trabalhado nas próximas aulas. 
+});
